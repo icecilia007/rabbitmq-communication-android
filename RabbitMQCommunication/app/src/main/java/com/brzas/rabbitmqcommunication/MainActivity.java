675920786cf487dbab1aef6c651b600f846ee190
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2023-06-28 14:19:48 -0300

    manifest and main class

--- RabbitMQCommunication/app/src/main/java/com/brzas/rabbitmqcommunication/MainActivity.java
+++ RabbitMQCommunication/app/src/main/java/com/brzas/rabbitmqcommunication/MainActivity.java
@@ -2,7 +2,10 @@ package com.brzas.rabbitmqcommunication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.brzas.rabbitmqcommunication.activities.RabbitMQActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, RabbitMQActivity.class));
     }
 }
